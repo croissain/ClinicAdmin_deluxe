bac30859d9c9c20273e9e3781dcbb4a536cd5dba
thole20042001 <nhattho03@gmail.com>
2022-05-27 10:17:05 +0700

    small UI fix

---
+++ ClinicAdmin/BUS/StatisticBUS.cs
@@ -0,0 +1,27 @@
+﻿using ClinicAdmin.DAO;
+using ClinicAdmin.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClinicAdmin.BUS
+{
+    public class StatisticBUS
+    {
+        private static StatisticBUS instance;
+
+        public static StatisticBUS getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new StatisticBUS();
+            }
+            return instance;
+        }
+    }
+}
